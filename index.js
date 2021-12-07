9573714fc4da5554dc51da525efb2ec2db0e7fe3
willchrist <william.christian@rga.com>
2021-12-07 15:54:10 +0700

    [will] fix missing dependency

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+var http = require('http');
 
 var indexRouter = require('./routes/index');
 var memberRouter = require('./routes/v1/memberRoutes');
